d337c77a607d41c046c40284b5480ace442cae28
chloekim917 <chloekim917@gmail.com>
2020-07-07 10:13:01 -0400

    updated controllers

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,28 +4,16 @@ class Api::V1::CategoriesController < ApplicationController
         render json: categories
     end
 
-    def create
-        category = Category.new(category_params)
-        category.save!
-        render json: category
-    end
-
     def update
         category = Category.find(params[:id])
         category.update!(category_params)
         render json: category
     end
 
-    def destroy
-        category = Category.find(params[:id])
-        category.destroy!
-        render json: {}
-    end
-
     private
 
     def category_params
-        params.require(:category).permit(:name)
+        params.require(:category).permit(:name, :budget_amount)
     end
 
 
