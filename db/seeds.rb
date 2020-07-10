895286c2f62fb79dc93c0a51c01695101bbb5f36
cathjime <xcatherine.jimenez@gmail.com>
2020-07-10 14:02:00 -0400

    reseeded for bar graph

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 Category.destroy_all
 Expenditure.destroy_all
 
-food = Category.create(name: 'Food', budget_amount:0 )
-utility = Category.create(name: 'Utility', budget_amount:0 )
-entertainment = Category.create(name: 'Entertainment', budget_amount:0)
-housing = Category.create(name: 'Housing', budget_amount:0)
-transportation = Category.create(name: 'Transportation', budget_amount:0)
-personal_care = Category.create(name: 'Personal Care', budget_amount:0)
-gift= Category.create(name: 'Gift', budget_amount:0)
-misc = Category.create(name: 'Miscellaneous', budget_amount:0)
-travel = Category.create(name: 'Travel', budget_amount:0)
-groceries = Category.create(name: 'Groceries', budget_amount:0)
-medical = Category.create(name: 'Medical', budget_amount:0)
+food = Category.create(name: 'Food', budget_amount:155 )
+utility = Category.create(name: 'Utility', budget_amount:150 )
+entertainment = Category.create(name: 'Entertainment', budget_amount:175)
+housing = Category.create(name: 'Housing', budget_amount:1400)
+transportation = Category.create(name: 'Transportation', budget_amount:143)
+personal_care = Category.create(name: 'Personal Care', budget_amount:100)
+gift= Category.create(name: 'Gift', budget_amount:50)
+misc = Category.create(name: 'Miscellaneous', budget_amount:30)
+travel = Category.create(name: 'Travel', budget_amount:7)
+groceries = Category.create(name: 'Groceries', budget_amount:275)
+medical = Category.create(name: 'Medical', budget_amount:80)
 
 
 Expenditure.create(date: 20200701, category_id: food.id, description:"bubble tea", amount: 5.0)
